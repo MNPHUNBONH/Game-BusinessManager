2a796d4bd72234524aca1e55c51d7b713e819a6c
Valery Korenko <karankovalery@gmail.com>
2024-08-21 20:49:30 +0300

    First commit

--- BusinessManager/obj/Debug/net8.0/BusinessManager.AssemblyInfo.cs
+++ BusinessManager/obj/Debug/net8.0/BusinessManager.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BusinessManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9762af4e5879068c8dc4bde9b63b5f99c8983d49")]
 [assembly: System.Reflection.AssemblyProductAttribute("BusinessManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BusinessManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
